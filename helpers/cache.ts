86e344af8c522a05eff83d1d2a6ddf49c563e6cd
Gleb Fokin <g.fokin@skyeng.ru>
2024-07-07 01:40:07 +0700

    Fix ts

--- helpers/cache.ts
+++ helpers/cache.ts
@@ -4,17 +4,21 @@ const cache = {};
 let cacheTimestamp: number = 0;
 
 export function resetCache(key: string) {
+  // @ts-expect-error
   cache[key] = undefined;
 }
 
 export function getCache(key: string, ttl = 60 * 1000) {
+  // @ts-expect-error
   if (cache[key] && Date.now() - cache[key].timestamp < ttl) {
+    // @ts-expect-error
     return cache[key].value;
   }
   return undefined;
 }
 
 export function setCache(key: string, value: any) {
+  // @ts-expect-error
   cache[key] = {
     value,
     timestamp: Date.now(),
