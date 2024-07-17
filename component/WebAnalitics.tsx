c9be0a256a831742dcdb6232d2a77a4978e7d38a
Gleb Fokin <g.fokin@skyeng.ru>
2024-07-17 23:00:01 +0700

    Добавляет ноу скрипт

--- component/WebAnalitics.tsx
+++ component/WebAnalitics.tsx
@@ -17,8 +17,9 @@ export function WebAnalitics() {
     ym(97857603, "hit", window.location.href);
   }, [pathName, searchParams]);
   return (
-    <Script id="yandex-metrika">
-      {`
+    <>
+      <Script id="yandex-metrika">
+        {`
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -31,6 +32,17 @@ export function WebAnalitics() {
              accurateTrackBounce:true
         });
       `}
-    </Script>
+      </Script>
+      <noscript>
+        <div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://mc.yandex.ru/watch/97857603"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+    </>
   );
 }
