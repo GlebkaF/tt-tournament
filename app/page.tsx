929b8956f29038d8510e926c81ed746b63746aec
Gleb Fokin <g.fokin@skyeng.ru>
2024-07-20 19:59:51 +0700

    Обновляет мету на главной

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,62 @@
-import MainPage from "@/component/MainPage";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export default function Home() {
-  return <MainPage></MainPage>;
+  return (
+    <div className="container pt-8">
+      <h1 className="text-3xl font-bold mb-4">
+        Настольной теннис. Европейский берег
+      </h1>
+
+      <div className="mb-8">
+        <p className="mt-2 text-gray-600">
+          Хотите играть с нами? Напишите в&nbsp;
+          <Link
+            href="https://t.me/glebkaf"
+            className="text-xl font-semibold text-blue-500 hover:underline"
+            target="_blank" // чтобы открывать ссылку в новом окне
+            rel="noopener noreferrer" // для безопасности
+            aria-label="Напишите в Telegram"
+          >
+            Telegram
+          </Link>
+          .
+        </p>
+      </div>
+
+      <h2 className="text-2xl font-bold mb-4">Новости</h2>
+
+      <div className="space-y-8">
+        <div>
+          <Link
+            href="/post/random-pairs-2024"
+            className="text-xl font-semibold text-blue-500 hover:underline"
+            aria-label="Результаты второго парного турнира Случайные пары 2024"
+          >
+            Случайные пары 2024
+          </Link>
+          <p className="mt-2 text-gray-600">
+            Результаты второго парного турнира &ldquo;Случайные пары
+            2024&rdquo;, который прошел 20 июля 2024 года.
+          </p>
+        </div>
+        {/* Вы можете добавлять больше новостей здесь */}
+      </div>
+    </div>
+  );
 }
 
+const title = "Настольный теннис. Европейский Берег";
+const description =
+  "Играем в настольный теннис на набережной Европейского Берега. Ежегодный летний турнир, ситуационные турниры.";
+
 export const metadata: Metadata = {
-  title: "Настольный теннис на Европейском Берегу",
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url: "https://ebtt.com",
+    type: "website",
+  },
 };
