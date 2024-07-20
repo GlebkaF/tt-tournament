9000f9253985e59656a998dc8f6bc3226217236f
Gleb Fokin <g.fokin@skyeng.ru>
2024-07-20 17:56:22 +0700

    Добавляет роботс и сайтмап

---
+++ app/robots.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/post/random-pairs-2024",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: "/standings/",
+      },
+      {
+        userAgent: "*",
+        disallow: "/matches/",
+      },
+    ],
+    sitemap: "https://ebtt.ru/sitemap.xml",
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ebtt.ru",
+    },
+    {
+      url: "https://ebtt.ru/post/random-pairs-2024",
+    },
+  ];
+}
