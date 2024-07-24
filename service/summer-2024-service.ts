367d6007b78024f031e4ada5d33afe13464494eb
Gleb Fokin <g.fokin@skyeng.ru>
2024-07-24 09:43:37 +0700

    Убирает Васильева

--- service/summer-2024-service.ts
+++ service/summer-2024-service.ts
@@ -3,8 +3,8 @@ import { getCache, resetCache, setCache } from "@/helpers/cache";
 import { Match, MatchResult, PrismaClient } from "@prisma/client";
 
 const summerTournament2024PlayersIds = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 26, 27, 28, 29, 30, 31, 32, 34, 35,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+  24, 26, 27, 28, 29, 30, 31, 32, 34, 35,
 ];
 
 const MATCHES_CACHE_KEY = "matches_cache";
