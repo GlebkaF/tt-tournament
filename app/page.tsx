3c161b9f538b6457dffcc0d1987a0d4423585d6b
Gleb Fokin <g.fokin@skyeng.ru>
2025-06-05 21:53:15 +0700

    Обновляет страницу главного экрана, изменяя текст и структуру контента о турнире по настольному теннису. Удалены устаревшие абзацы, добавлена информация о летнем турнире 2025 года и изображение победителей турнира 2024 года.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Link from "next/link";
 import thisisus from "@/images/thisisus.jpg";
+
 import Image from "next/image";
 import createDeps from "@/service/create-deps";
 
@@ -16,44 +17,14 @@ export default async function Home() {
         {/* Column for Text Content */}
         <div className="flex-1">
           <div className="space-y-4">
-            <h1 className="heading-l mb-16">
-              Настольный теннис. Европейский берег
-            </h1>
-            <Image
-              src={thisisus}
-              alt="Заглушка для фотографии"
-              className="object-cover mb-12 w-full"
-            />
+            <h1 className="heading-l">Настольный теннис. Европейский берег.</h1>
+
             <p className="text-gray-600">
-              Мы — сообщество любителей настольного тенниса в ЖК «Европейский
-              Берег». Наш основатель, Женя Шкретов, прекрасно владеет игрой и с
-              удовольствием помогает новичкам освоиться и повысить свой уровень.
+              Летний турнир 2025 стартовал третьего июня! Групповая стадия
+              продлится 10 недель, а в конце лета состоятся финалы в трех лигах:
+              «Золотая», «Серебряная» и «Бронзовая».
             </p>
-            <p className="text-gray-600">
-              Сообщество существует уже три года. В этом году мы проводим третий
-              ежегодный летний турнир, участие в котором принимают 42 человека.
-              В прошлые годы турнир собирал по 32 и 20 участников.
-            </p>
-            <p className="text-gray-600">
-              Наши мероприятия разнообразны: от длительных летних турниров до
-              однодневных соревнований. Летний турнир включает групповую стадию,
-              которая длится 10 недель, а в конце лета мы играем финалы в трех
-              лигах: «Золотая», «Серебряная» и «Бронзовая». Однодневные турниры
-              бывают одиночные и парные.
-            </p>
-            <p className="text-gray-600">
-              Мы ценим свою независимость, но поддерживаем хорошие отношения с
-              «ТОС Европейский берег», местными депутатами, управляющей
-              компанией и другими спортивными сообществами ЕБ. Победители
-              турниров получают грамоты, медали и иногда призы от местных
-              заведений.
-            </p>
-            <p className="text-gray-600">
-              Наша сила — в дружелюбной и поддерживающей атмосфере. Мы не только
-              играем в теннис, но и регулярно собираемся на различные
-              мероприятия и встречи, что помогает укреплять нашу общность. Зимой
-              мы не прекращаем играть, находя столы в залах.
-            </p>
+
             <p className="mt-2 text-gray-600">
               Хотите играть с нами? Присоединяйтесь в&nbsp;
               <Link
@@ -67,26 +38,13 @@ export default async function Home() {
               </Link>
               .
             </p>
-          </div>
-        </div>
-        {/* Column for News Section */}
-        <div className="w-full desktop:w-1/3 mt-8">
-          <h2 className="heading-m font-bold mb-24">Новости</h2>
-          <div className="flex flex-col gap-12">
-            {posts.map((post) => {
-              return (
-                <div key={post.id}>
-                  <Link
-                    href={`/post/${post.slug}`}
-                    className="text-l"
-                    aria-label="Финалы летнего турнира 2024. День 1"
-                  >
-                    {post.title}
-                  </Link>
-                  <p className="text-secondary-base">{post.description}</p>
-                </div>
-              );
-            })}
+            <br />
+
+            <img
+              src="/image/post/summer-finals-2024-day-2/winners.png"
+              alt="Победители 2024"
+              className="object-fit mb-12 w-full"
+            />
           </div>
         </div>
       </div>
